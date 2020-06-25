c5c41e5a770b8873d3d525a27cce2d4c4db1a2cf
Kyle Insaurralde <kinsaurralde@yahoo.com>
2020-06-25 22:39:29 +0000

    resize and center cat images

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hi', 'Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'Greeting', "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."];
+    ['Hi', 'Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'Greeting', "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -30,33 +30,62 @@ function addRandomGreeting() {
 let cat_photo_num = 0;
 let cat_gallery_image = document.getElementById("cat-gallery-image");
 let cat_gallery_subtitle = document.getElementById("cat-gallery-subtitle");
-const cat_photos = [
-    ["simba.jpg", "This is Simba sleeping part 1"],
-    ["simba_2.jpg", "This is Simba sleeping part 2"],
-    ["IMG_7812.JPG", "This is Simba sleeping part 3"],
-    ["IMG_6657.jpeg", "This is Simba sleeping part 4"],
+let cat_photos = [
+  ["simba.jpg", "This is Simba sleeping part 1", null],
+  ["simba_2.jpg", "This is Simba sleeping part 2", null],
+  ["IMG_7812.JPG", "This is Simba sleeping part 3", null],
+  ["IMG_6657.jpeg", "This is Simba sleeping part 4", null],
 ];
+const max_width = 800;
+const max_height = 600;
+
+changeCatPhoto(0);
 
 function changeCatPhoto(num) {
-    cat_gallery_image.src = "/images/" + cat_photos[num][0];
-    cat_gallery_subtitle.innerText = cat_photos[num][1];
+  cat_gallery_subtitle.innerText = cat_photos[num][1];
+  cat_gallery_image.innerHTML = "";
+  if (cat_photos[num][2] == null) {
+    let new_image = new Image();
+    new_image.onload = function() {
+      let width = new_image.width;
+      let height = new_image.height;
+      if (width / height > max_width / max_height) {
+        let scale_factor = max_width / width;
+        let new_height = height * scale_factor;
+        new_image.style.width = max_width + "px";
+        new_image.style.height = new_height + "px";
+        new_image.style.marginTop = ((max_height - new_height) / 2) + "px";
+      } else {
+        let scale_factor = max_height / height;
+        let new_width = width * scale_factor;
+        new_image.style.width = new_width + "px";
+        new_image.style.height = max_height + "px";
+        new_image.style.marginLeft = ((max_width - new_width) / 2) + "px";
+      }
+      cat_photos[num][2] = new_image;
+      cat_gallery_image.appendChild(new_image);
+      }
+    new_image.src = "/images/" + cat_photos[num][0];
+  } else {
+    cat_gallery_image.appendChild(cat_photos[num][2]);
+  }
 }
 
 function previousCatPhoto() {
-    if (cat_photo_num > 0) {
-        cat_photo_num -= 1;
-    } else {
-        cat_photo_num = cat_photos.length - 1;
-    }
-    changeCatPhoto(cat_photo_num);
+  if (cat_photo_num > 0) {
+    cat_photo_num -= 1;
+  } else {
+    cat_photo_num = cat_photos.length - 1;
+  }
+  changeCatPhoto(cat_photo_num);
 }
 
 function nextCatPhoto() {
-    cat_photo_num = (cat_photo_num + 1) % cat_photos.length;
-    changeCatPhoto(cat_photo_num);
+  cat_photo_num = (cat_photo_num + 1) % cat_photos.length;
+  changeCatPhoto(cat_photo_num);
 }
 
 function randomCatPhoto() {
-    cat_photo_num = Math.floor(Math.random() * cat_photos.length);
-    changeCatPhoto(cat_photo_num);
+  cat_photo_num = Math.floor(Math.random() * cat_photos.length);
+  changeCatPhoto(cat_photo_num);
 }
